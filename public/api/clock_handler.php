9f9fd5b3df7d383d7e31d5d98577febb68295f63
ZaaidahAbra <zaaidahabrahams2001@gmail.com>
2025-11-10 14:29:52 +0200

    Update clock_handler.php

--- public/api/clock_handler.php
+++ public/api/clock_handler.php
@@ -5,7 +5,7 @@ header("Pragma: no-cache");
 header("Expires: 0");
 
 session_start();
-require_once __DIR__ . '/../../app/includes/db.php';
+require_once __DIR__ . '/../../includes/db.php';
 
 header('Content-Type: application/json');
 
